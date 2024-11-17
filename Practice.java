a8775d3fe0af5c347e52b1d53aee2492c163f4c4
Dhiraj Patil <Dhirajp88055@gmail.com>
2024-11-17 16:21:56 +0530

    Initial commit

---
+++ Practice.java
@@ -0,0 +1,7 @@
+public class Practice{
+    public static void main(String[] args) {
+        
+        System.out.println("Hi, I am Dhiraj Patil");
+        System.out.println("How are you?");
+    }
+}
